91f843d102b7b51f4b9e0e969af157542c60e903
Benne <bennesantana@hotmail.com>
2026-03-17 20:09:16 -0300

    Ajuste Mappering 2x

--- OsLog.Application/DTOs/Empresa/UnidadeDto.cs
+++ OsLog.Application/DTOs/Empresa/UnidadeDto.cs
@@ -1,14 +1,17 @@
-﻿namespace OsLog.Application.DTOs.Unidade;
+﻿using OsLog.Domain.Enums;
+
+namespace OsLog.Application.DTOs.Unidade;
 
 public class UnidadeDto
 {
     public int Id { get; set; }
-    public int EmpresaId { get; set; }
+    public Guid EmpresaId { get; set; }
     public string Nome { get; set; } = string.Empty;
     public string? Cnpj { get; set; }
     public string? InscricaoEstadual { get; set; }
     public string? InscricaoMunicipal { get; set; }
     public string? Endereco { get; set; }
     public string? Telefone { get; set; }
-    public bool Ativa { get; set; } // Corresponde a !FlExcluido
+    public TipoUnidade Tipo { get; set; }
+    public bool Ativa { get; set; }
 }
---
+++ OsLog.Application/Mapping/UnidadeProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using OsLog.Application.DTOs.Unidade;
+using OsLog.Domain.Entities;
+
+namespace OsLog.Application.Mapping;
+
+public class UnidadeProfile : Profile
+{
+    public UnidadeProfile()
+    {
+        CreateMap<Unidade, UnidadeDto>();
+
+        CreateMap<UnidadeCreateDto, Unidade>()
+            .ForMember(d => d.Id, opt => opt.Ignore())
+            .ForMember(d => d.EmpresaId, opt => opt.Ignore())
+            .ForMember(d => d.Tipo, opt => opt.Ignore())
+            .ForMember(d => d.Ativa, opt => opt.Ignore())
+            .ForMember(d => d.DataCriacaoUtc, opt => opt.Ignore())
+            .ForMember(d => d.Empresa, opt => opt.Ignore())
+            .ForMember(d => d.UsuariosAcesso, opt => opt.Ignore())
+            .ForMember(d => d.Tipo, opt => opt.Ignore());
+    }
+}
